0df05e4c1036b16818bf32c4596669d6a1e64fe0
semvs <semvs@ayvansaray.edu.tr>
2019-01-02 09:34:45 +0300

    gün farki

---
+++ oop1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TarihBİlgisi t = new TarihBİlgisi();
+            t.GecerliTarih = "30.12.2018";
+            Console.WriteLine(t.GunFarki(DateTime.Today.ToString()));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ oop1/TarihBİlgisi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop1
+{
+    public class TarihBİlgisi
+    {
+        private DateTime tarih;
+        public string GecerliTarih
+        {
+            set { tarih = Convert.ToDateTime(value); }
+        }
+        private double GunOlarakTarihFarki(DateTime farkAlinacakTarih)
+        {
+            return (farkAlinacakTarih - tarih).TotalDays;
+        }
+        public string GunFarki(string YeniTarih)
+        {
+            return GunOlarakTarihFarki(Convert.ToDateTime(YeniTarih)).ToString() + "gun fark var";
+        }
+
+    }
+}
